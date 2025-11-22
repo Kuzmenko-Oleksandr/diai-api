1831f170ceed6fdf5159d45862512f4db21b5182
Sofiia Trokhymchuk <sofiia.trokhymchuk@binary-studio.com>
2025-11-22 13:56:43 +0200

    feat: increase file size limit

--- src/plugins/multipart.ts
+++ src/plugins/multipart.ts
@@ -5,5 +5,8 @@ import fp from "fastify-plugin";
 export default fp<FastifyMultipartOptions>(async (fastify, _opts) => {
 	await fastify.register(multipart, {
 		attachFieldsToBody: "keyValues",
+		limits: {
+			fileSize: 10 * 1024 * 1024,
+		},
 	});
 });
